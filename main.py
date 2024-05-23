38ae10d07c8ad29f0fd8e015570ac1052cb7526b
Akshay P <akshayp@ontash.net>
2024-05-23 13:51:14 +0530

    Translator added in the code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from greets import greetings
+from translate import Translator
+
+mytranslator = Translator(to_lang='pt')
 
 for i in greetings:
+    i = mytranslator.translate(i)
     print(i.title() + "!" + "....blaaaaaah")
-    print("\n")
-    print("Life changes")
